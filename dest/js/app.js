ce51bac69b6062abf800e9577041788ed66265b0
Andrii <Luckyhidalgo@gmail.com>
2020-08-08 10:21:35 +0300

    Homepage - created media sectiona - w\o PP & adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -35,6 +35,52 @@ var initPreventBehavior = function initPreventBehavior() {
 };
 
 /**
+ * @name initSwiper
+ *
+ * @description initialize Swiper
+ */
+var initSwiper = function initSwiper() {
+
+	new Swiper('.mediaCarousel', {
+		loop: true,
+		grabCursor: true,
+		freeMode: false,
+		effect: 'slide',
+		speed: 800,
+		slidesPerView: 3,
+		spaceBetween: 98,
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 3,
+				spaceBetween: 98
+			},
+			// when window width is >= 480px
+			480: {
+				slidesPerView: 3,
+				spaceBetween: 98
+			},
+			// when window width is >= 640px
+			640: {
+				slidesPerView: 3,
+				spaceBetween: 98
+			}
+		},
+		navigation: {
+			nextEl: '.media__slider-btn--next',
+			prevEl: '.media__slider-btn--prev'
+		},
+		on: {
+			init: function init() {
+				$('.media__slider').css({
+					opacity: 1
+				});
+			}
+		}
+	});
+};
+
+/**
  * @description Document DOM ready.
  */
 (function () {
@@ -57,6 +103,7 @@ var initPreventBehavior = function initPreventBehavior() {
 		// ==========================================
 
 		// lib
+		initSwiper();
 		// ==========================================
 
 		// callback
