8cb58987d4034da2bb7623619cc091724f3fc2e3
Andrii <Luckyhidalgo@gmail.com>
2020-08-08 11:19:33 +0300

    Homepage - created contacts & footer section - w\o PP & adaptive.

---
+++ src/js/_lib/swiper.js
@@ -0,0 +1,47 @@
+
+
+/**
+ * @name initSwiper
+ *
+ * @description initialize Swiper
+ */
+const initSwiper = () => {
+
+  new Swiper('.mediaCarousel', {
+    loop: true,
+    grabCursor: true,
+    freeMode: false,
+    effect: 'slide',
+		speed: 800,
+    slidesPerView: 3,
+    spaceBetween: 98,
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 3,
+				spaceBetween: 98
+			},
+			// when window width is >= 480px
+			480: {
+				slidesPerView: 3,
+				spaceBetween: 98
+			},
+			// when window width is >= 640px
+			640: {
+				slidesPerView: 3,
+				spaceBetween: 98
+			}
+		},
+    navigation: {
+      nextEl: '.media__slider-btn--next',
+      prevEl: '.media__slider-btn--prev',
+    },
+		on: {
+    	init: function() {
+				$('.media__slider').css({
+					opacity: 1
+				});
+			}
+		}
+  });
+};
